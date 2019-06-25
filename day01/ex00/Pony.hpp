8e29cab51ccfda1d490cc57bfd8b4eff6e5375ef
AnnyDanny <anndann2201@gmail.com>
2019-06-25 13:47:57 +0300

    day01_start

---
+++ day01/ex00/Pony.hpp
@@ -0,0 +1,16 @@
+#ifndef PONY_HPP
+#define PONY_HPP
+
+#include <iostream>
+#include <cstdlib>
+
+class Pony{
+public:
+	Pony(std::string str1, std::string name, bool can_fly);
+	~Pony();
+	std::string str1;
+	std::string name:
+	bool can_fly;
+};
+
+#endif
